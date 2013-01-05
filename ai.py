c24caeedd929885587fc3fe3d344778d06d15b05
wong2 <wonderfuly@gmail.com>
2013-01-05 10:36:39 +0800

    添加教学模式，未果

--- ai.py
+++ ai.py
@@ -5,6 +5,31 @@ from simsimi import SimSimi
 
 simi = SimSimi()
 
+def my_logic(text):
+    # 教学模式: /Q python和ruby哪个好？ /A php最好！
+    if '/Q' in text and '/A' in text:
+        _, right_part  = text.split('/Q', 1) 
+        question, answer = [s.strip() for s in right_part.split('/A', 1)]
+        if question and answer:
+            simi.teach(question, answer)
+            return '学会啦'
+
+    return None
+
 # some magic here
 def magic(text):
-    return simi.chat(text).encode('utf-8')
+    text = text.strip()
+
+    # 先自己试着处理，否则给simsimi
+    try:
+        my_result = my_logic(text)
+    except:
+        my_result = None
+
+    if my_result:
+        return my_result
+    else:
+        return simi.chat(text).encode('utf-8')
+
+if __name__ == '__main__':
+    print magic('/Q 今天天气怎么样？ /A 大雪')
