352081fbe23425cd1cb381d8e8b132c8f46a3b30
Qijiang Fan <fqj1994@gmail.com>
2013-01-09 20:55:20 +0800

    fix sql mistake

--- mysql.sql
+++ mysql.sql
@@ -7,4 +7,4 @@ CREATE TABLE `question_and_answers` (
  PRIMARY KEY (`id`),
  KEY `time` (`time`),
  KEY `worker` (`worker`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
