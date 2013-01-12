a519713fbbe83f832bf30edb11eb87c08e1ae35a
huangqimin <huangqimin@baidu.com>
2013-01-12 20:29:02 +0800

    verify city in function test and change some code

--- plugins/weather.py
+++ plugins/weather.py
@@ -29,31 +29,35 @@ import requests
 import cPickle as pickle
 
 
+def city(data):
+    cityidDict = pickle.load(file('./data/cityid', 'r'))
+    for city in cityidDict:
+        if city.encode('utf8') in data['message']:
+            return True
+    return False
+
+
 def test(data, bot):
-    return '天气' in data['message']
+    return '天气' in data['message'] and city(data)
 
 
 def weather(cityid):
-    weatherinfo = requests.get('http://www.weather.com.cn/data/cityinfo/' + cityid + '.html').json()['weatherinfo']
     try:
+        weatherinfo = requests.get('http://www.weather.com.cn/data/cityinfo/' + cityid + '.html').json()['weatherinfo']
         return (weatherinfo['city'] + ', ' + weatherinfo['weather'] + ', ' + weatherinfo['temp1'] + ' ~ ' + weatherinfo['temp2']).encode('utf8')
     except:
         return 0
 
 
 def handle(data, bot):
-    # 加载城市名称和城市id
     cityidDict = pickle.load(file('./data/cityid', 'r'))
-    cityFlag = False
     for city in cityidDict:
         if city.encode('utf8') in data['message']:
             reply = weather(cityidDict[city])
-            cityFlag = True
-            break
-    return reply if cityFlag and reply else '不会自己去看天气预报啊'
+            return reply if reply else '不会自己去看天气预报啊'
 
 
 if __name__ == '__main__':
     print test({'message': '天气怎么样'}, None)
-    print handle({'message': '天气怎么样', 'author_id': 'HQM'}, None)
+    print test({'message': '北京天气怎么样'}, None)
     print handle({'message': '北京天气怎么样', 'author_id': 'HQM'}, None)
